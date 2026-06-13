3c8bd85bb4502affb76feb287594e7c29e5728ee
mikedcf <mikedcf74@gmail.com>
2026-06-13 17:48:04 -0300

    teste para encontrar o erro do cargo streamer

--- main.py
+++ main.py
@@ -192,6 +192,7 @@ async def MostrarOuAtualizarPerfil(interaction: discord.Interaction):
             allstar_url = dadosUserCompleto['data']['userDados'][0]['allstar_url']
             total_medalhas = dadosUserCompleto['data']['userDados'][0]['total_medalhas']
             total_destaques = dadosUserCompleto['data']['userDados'][0]['total_destaques']
+            print(organizador)
 
             if gerencia == 'streammer':
                 gerencia = 'streamer'
@@ -418,7 +419,7 @@ async def MostrarOuAtualizarPerfil(interaction: discord.Interaction):
                                         novoApelido = novoApelido +'🛡️'
                                         await interaction.user.add_roles(interaction.guild.get_role(MODERADOR))
                                     elif gerencia == 'streamer':
-                                        print(STREAMER)
+                                        
                                         novoApelido = novoApelido + '🎥'
                                         await interaction.user.add_roles(interaction.guild.get_role(STREAMER))
 
